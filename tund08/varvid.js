90582158d9ee2410f45f83331177b24b13afadac
Misasja <reiteruljas@gmail.com>
2022-05-10 17:23:43 +0300

    tund09

--- tund08/varvid.js
+++ tund08/varvid.js
@@ -1,17 +1,54 @@
-let sisendTekst = prompt("Sisesta enda lemmikvärv")
-let valjundElement = document.getElementById("valjund")
+let arvatudVarvid = []
 
-if (sisendTekst == "roheline"  ) {
-    let valjastatavTekst = "Palju õnne arvasid ära minu lemmikvärvi."
-    valjundElement.innerHTML += "<div>" + valjastatavTekst + "</div>"
-    valjundElement.innerHTML += `<img src="apelsin.jpg" width="300" alt="apelsin">`
 
-} else {
-    valjundElement.innerHTML = "<div>värv " + sisendTekst + " ei ole minu lemmikvärv</div>"
-    for (let index = 0; index < 5; index++) {
-        valjundElement.innerHTML += "<div>" + (index + 1) + `. ` + sisendTekst + "</div>"
+function valjastaTeade(teade) {
+    let valjundEl = document.getElementById("valjund")
+    console.log(teade)
+    valjundEl.innerHTML += "<div>" + teade + "</div>"
+}
+
+function valjastaArvamisteAjalugu() {
+    let valjundEl = document.getElementById("valjund")
+    valjundEl.innerHTML = "<h3>Värvide arvamiste ajalugu</h3>"
+    for (let index = 0; index < arvatudVarvid.length; index++) {
+        valjundEl.innerHTML += `<div>${index}. arvamine - ${arvatudVarvid[index]}</div>`
     }
 }
 
+function varviArvamine() {
+    console.log("Programm alustas tööd")
+    let sisendElement = document.getElementById("sisend")
+    let sisendTekst = sisendElement.value
+    let valjundElement = document.getElementById("valjund")
+    valjundElement.innerHTML = ``
+    arvatudVarvid.push(sisendTekst)
+    valjastaArvamisteAjalugu()
 
-varviArvamine()
+    const lemmikVarvid = [
+        "roheline",
+        "sinine",
+        "punane",
+        "roosa"
+    ]
+
+    if (lemmikVarvid.includes(sisendTekst)) {
+        valjastaTeade("Õnnitlused - arvasid ära minu lemmikvärvi " + sisendTekst)
+        valjundElement.innerHTML += `<img src="apelsin.jpg" width="300" alt="apelsin">`
+    }
+
+    else if (!sisendTekst) {
+        valjastaTeade("Sisesta värv")
+    } else {
+        valjundElement.innerHTML = "<div>värv " + sisendTekst + " ei ole minu lemmikvärv</div>"
+        for (let index = 0; index < 5; index++) {
+            valjundElement.innerHTML += "<div>" + (index + 1) + `. ` + sisendTekst + "</div>"
+        }
+    }
+
+    console.log("Programm lõpetas töö")
+
+}
+
+function valjastaRuut(x) {
+    console.log(x * x)
+}
