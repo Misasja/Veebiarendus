442c8f31b5e625f933ee872794e67e27178db3db
Misasja <reiteruljas@gmail.com>
2022-05-29 16:34:04 +0300

    tund13

---
+++ tund11/kodutoo.js
@@ -0,0 +1,23 @@
+(function(){
+    new Vue({
+      el: '#todo-list',
+      data: {
+        items: [],
+        todoText: ''
+      },
+      methods: {
+        add: function(){
+          this.items.push({
+            text: this.todoText
+          });
+          this.todoText = '';
+        },
+        'delete': function(ev){
+          this.items.remove(ev.targetVM.$data);
+        },
+        deleteAll: function(){
+          this.items = [];
+        }
+      }
+    });
+  }).call(this);
