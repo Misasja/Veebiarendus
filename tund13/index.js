eb295dc9bc6c09316eb26088cb9f7dbb5071f8df
Misasja <reiteruljas@gmail.com>
2022-05-29 16:37:51 +0300

    tund13

--- tund13/index.js
+++ tund13/index.js
@@ -1,32 +1,43 @@
-let ylesanne1 = {kirjeldus: `tee hommikuvõimlemist`, kasTehtud: true, tahtsus: 8, tahtaeg: `enne kella 8-t`}
-let ylesanne2 = {kirjeldus: `jaluta koeraga`, kasTehtud: false, tahtsus: 8, tahtaeg: `enne kella 8-t`}
+let ylesanne1 = {kirjeldus: 'tee hommikuvõimlemist', kasTehtud: true, tahtsus: 8, tahtaeg: 'enne kella 8-t'}
+let ylesanne2 = {kirjeldus: 'jaluta koeraga', kasTehtud: false, tahtsus: 8, tahtaeg: 'enne kella 8-t'}
 console.log(ylesanne1)
 
 //koonda ylesanded massiivi
 let ylesanded = [ylesanne1, ylesanne2]
 
-ylesanded.push({kirjeldus: `tee hommikusöök`, kasTehtud: false, tahtsus: 8, tahtaeg: `enne kella 8-t`})
+ylesanded.push({kirjeldus: 'tee hommikusöök', kasTehtud: false, tahtsus: 8, tahtaeg: 'enne kella 8-t'})
 
 //koosta ühe ülesande kirjelduse html element
 function tagastaYlesanneHtml(index) {
-    let tehtud = ``
-    let tehtudKlass = ``
+    let tehtud = ''
+    let tehtudKlass = ''
     if (ylesanded[index].kasTehtud) {
-        tehtud = `checked`
-        tehtudKlass = `class="tehtud"`
+        tehtud = 'checked'
+        tehtudKlass = 'class="tehtud"'
     }
     return `
-    <div ${tehtudKlass} >
+    <div ${tehtudKlass}>
         <input type="checkbox" ${tehtud}>
-        Oluline ülesanne: ${ylesanded[index].kirjeldus} ${ylesanded[index.tahtaeg]}
+        Oluline ülesanne: ${ylesanded[index].kirjeldus} ${ylesanded[index].tahtaeg}
     </div>
     `
-    //ülemine template tagastab sarnase väärtuse:
-    //return `<div><input type="checkbox">Oluline ülesanne ` + ylesanded[index].kirjeldus + ` ` + ylesanded[index]. tahtaeg + `</div>`
+    //Ülemine template tagastab sarnase väärtuse:
+    //return '<div><input type="checkbox" >Oluline ülesanne: ' + ylesanded[index].kirjeldus + //' ' + ylesanded[index].tahtaeg + '</div>'
 }
 
 //tee tsükkel mis väljastaks kõik objektid massiivist
-const ylesandedElement = document.getElementById(`ylesanded`)
+const ylesandedElement = document.getElementById('ylesanded')
 for (let i = 0; i < ylesanded.length; i++) {
-ylesandedElement.innerHTML += tagastaYlesanneHtml(i)
+   ylesandedElement.innerHTML += tagastaYlesanneHtml(i)
+}
+
+//funktsioon ülesande andmete lugemiseks ja massiivi lisamiseks
+function lisaYlesanne() {
+    const ylesanne = document.getElementById('kirjeldus')
+    const tahtaeg = document.getElementById('aeg')
+    const uusYlesanne = {kirjeldus: ylesanne.value, kasTehtud: false, tahtsus: 8, tahtaeg: tahtaeg.value }
+    ylesanded.push(uusYlesanne)
+    ylesandedElement.innerHTML += tagastaYlesanneHtml(ylesanded.length - 1) 
+    ylesanne.value = ''
+    tahtaeg.value = ''
 }
